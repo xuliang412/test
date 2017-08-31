5d73360718845c838aacc028ce52b5152448aecc
xuliang <1209066811@qq.com>
2017-08-31 15:47:47 +0800

    添加一个add方法

---
+++ TestGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public void add(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
